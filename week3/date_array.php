0e9cfdf826ebdb47848f90f383c92c58628eec40
Timothy Poon Bing Jie <timothypoon1015@e.newera.edu.my>
2022-10-17 16:34:54 +0800

    homework

---
+++ week3/date_array.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Homework 2</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+
+<body>
+ 
+<div class="text-center">
+    <h1>What Is Your Date Of Birth?</h1>
+</div>
+<div class="d-flex">
+    
+<select class="form-select bg-info" aria-label="Default select example">
+  <option selected>Day</option>
+  <?php
+    for ($num = 1; $num <= 31; $num++) {
+        echo "<option value=\"$num\">$num</option>";
+      } 
+       ?>
+</select>
+
+<select class="form-select bg-warning" aria-label="Default select example">
+  <option selected>Month</option>
+  <?php
+  
+    $month = array("January", "February", "March", "April", "May", "June", "Jully", "August", "September", "October", "November", "December");
+
+    for ($num = 0; $num <= 11; $num++) {
+        echo "<option value=\"$num\">$month[$num]</option>";
+      } 
+       ?>
+</select>
+
+<select class="form-select bg-danger" aria-label="Default select example">
+  <option selected>Year</option>
+  <?php
+    for ($num = 2022; $num >= 1900; $num--) {
+        echo "<option value=\"$num\">$num</option>";
+      } 
+       ?>
+</select>
+
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
