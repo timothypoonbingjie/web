22a6cb3283c25e0367fae2485e5fbee9f39ffa18
Timothy Poon Bing Jie <timothypoon1015@e.newera.edu.my>
2022-10-31 17:10:40 +0800

    added Read One Customer’s Details, Read One Product’s Details

---
+++ project/product_read_one.php
@@ -0,0 +1,93 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>PDO - Read One Record - PHP CRUD Tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Product</h1>
+        </div>
+
+        <!-- PHP read one record will be here -->
+
+        <?php
+        // get passed parameter value, in this case, the record ID
+        // isset() is a PHP function used to verify if a value is there or not
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+        } else {
+            die('ERROR: Record ID not found.');
+        }
+
+        $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
+
+        //include database connection
+        include 'config/database.php';
+
+        // read current record's data
+        try {
+            // prepare select query
+            $query = "SELECT id, name, description, price FROM products WHERE id = :id ";
+            $stmt = $con->prepare($query);
+
+            // Bind the parameter
+            $stmt->bindParam(":id", $id);
+
+            // execute our query
+            $stmt->execute();
+
+            // store retrieved row to a variable
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // values to fill up our form
+            $name = $row['name'];
+            $description = $row['description'];
+            $price = $row['price'];
+            // shorter way to do that is extract($row)
+        }
+
+        // show error
+        catch (PDOException $exception) {
+            die('ERROR: ' . $exception->getMessage());
+        }
+        ?>
+
+
+
+        <!-- HTML read one record table will be here -->
+        <!--we have our html table here where the record will be displayed-->
+        <table class='table table-hover table-responsive table-bordered'>
+            <tr>
+                <td>Name</td>
+                <td><?php echo htmlspecialchars($name, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Description</td>
+                <td><?php echo htmlspecialchars($description, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td>Price</td>
+                <td><?php echo htmlspecialchars($price, ENT_QUOTES);  ?></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <a href='product_read.php' class='btn btn-danger'>Back to read products</a>
+                </td>
+            </tr>
+        </table>
+
+
+    </div> <!-- end .container -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
