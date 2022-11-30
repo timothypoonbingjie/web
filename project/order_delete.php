e683c072e0f1025d93a3479a0857b0c3562fe736
Timothy Poon Bing Jie <timothypoon1015@e.newera.edu.my>
2022-12-01 01:58:37 +0800

    added the delete function

---
+++ project/order_delete.php
@@ -0,0 +1,27 @@
+<?php
+// include database connection
+include 'config/database.php';
+try {     
+    // get record ID
+    // isset() is a PHP function used to verify if a value is there or not
+    $id=isset($_GET['id']) ? $_GET['id'] :  die('ERROR: Record ID not found.');
+
+    // delete query
+    $query = "DELETE FROM order_summary WHERE id = ?;DELETE FROM order_detials WHERE order_id = ?";
+    $stmt = $con->prepare($query);
+    $stmt->bindParam(1, $id);
+    $stmt->bindParam(2, $order_id);
+     
+    if($stmt->execute()){
+        // redirect to read records page and
+        // tell the user record was deleted
+        header('Location: order_read.php?action=deleted');
+    }else{
+        die('Unable to delete record.');
+    }
+}
+// show error
+catch(PDOException $exception){
+    die('ERROR: ' . $exception->getMessage());
+}
+?>
