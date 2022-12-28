b1706b722f47319557102e911149a3cd4d61a5c7
Timothy Poon Bing Jie <timothypoon1015@e.newera.edu.my>
2022-12-29 03:29:27 +0800

    redo topnav

--- project/topnav.php
+++ project/topnav.php
@@ -1,49 +1,45 @@
-<div class="container">
+<nav class="navbar sticky-top bg-body-tertiary navbar-expand-lg navbar-light bg-info">
 
-        <nav class="navbar navbar-expand-lg bg-info">
-
-            <a class="navbar-brand mx-3 " href="index.php">Home</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Customer
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="create_customers.php">create customer</a></li>
-                            <li><a class="dropdown-item" href="customers_read.php">read customer</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Order
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="create_new_order.php">create order</a></li>
-                            <li><a class="dropdown-item" href="order_summary.php">order list</a></li>
-                            
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Product
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="product_create.php">create product</a></li>
-                            <li><a class="dropdown-item" href="product_read.php">read product</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact_us.php">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="log_out.php">Log Out</a>
-                    </li>
+    <a class="navbar-brand bg-success rounded-4 m-3 p-2 text-white" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse m-3 justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown m-2">
+                <a class="nav-link dropdown-toggle bg-primary rounded-3 text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Customer
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="create_customers.php">create customer</a></li>
+                    <li><a class="dropdown-item" href="customers_read.php">read customer</a></li>
                 </ul>
-
-            </div>
-
-        </nav>
+            </li>
+            <li class="nav-item dropdown m-2">
+                <a class="nav-link dropdown-toggle bg-primary rounded-3 text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Order
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="create_new_order.php">create order</a></li>
+                    <li><a class="dropdown-item" href="order_summary">order summary</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown bg-primary rounded-3 m-2">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Product
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="product_create.php">create product</a></li>
+                    <li><a class="dropdown-item" href="product_read.php">read product</a></li>
+                </ul>
+            </li>
+            <li class="nav-item bg-primary rounded-3 m-2">
+                <a class="nav-link text-white" href="contact_us.php">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+    <div class="px-3">
+        <a href="log_out.php">Log Out</a>
+        <a href="index.php" class="p-3"><img src="image/TP.jpg" height="50px"></a>
+    </div>
+</nav>
