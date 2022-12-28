ce287f0874a8f49c5708671c9e5a9b820064aa0a
Timothy Poon Bing Jie <timothypoon1015@e.newera.edu.my>
2022-12-29 03:28:51 +0800

    added email function

--- project/contact_us.php
+++ project/contact_us.php
@@ -1,60 +1,80 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="en" dir="ltr">
 
 <head>
-    <title>Contact</title>
 
-    <title>PDO - Read Records - PHP CRUD Tutorial</title>
-    <!-- Latest compiled and minified Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <link href="css/style.css" rel="stylesheet" />
+    <meta charset="utf-8">
+    <title>Send Email</title>
+
+
 
 </head>
 
+
+
 <body>
+<title>Contact us</title>
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link href="css/style.css" rel="stylesheet" />
 
     <?php
     include 'topnav.php'
     ?>
     <div class="container-fluid image" style="background-image:url('image/bright2.png')">
-        <div class="d-flex justify-content-center mt-5">
-            <h1>Contact Us</h1>
-        </div>
+    <form class="d-flex flex-column mb-3 justify-content-center" action="contact_us.php" method="post">
+        Email <input type="email" name="email" value="" class='form-control' placeholder="Please enter your email"> <br>
+        Title <input type="text" name="subject" value="" class='form-control' placeholder="Please enter your Title"> <br>
+        Message <input type="text" name="message" value="" class='form-control' placeholder="Please enter your message"> <br>
+        <div class="d-flex justify-content-center mb-4"><button type="submit" class="btn btn-success" name="send">Send</button></div>
+    </form>
 
-        <div class="row m-5">
-            <div class="col">
-                Email:<br>
-                timothypoon1015@e.newera.edu.my
-            </div>
-            <div class="col">
-                Phone:<br>
-                013-9331358
-            </div>
-            <div class="col">
-                Instagram:<br>
-                zhe_1026
-            </div>
-        </div>
+        
 
-        <div class="d-flex justify-content-center mt-5">
-            <h1>Suggestion</h1>
-        </div>
+        <?php
 
-        <form>
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name">
-            </div>
-            <div class="mb-3">
-                <label for="suggestion" class="form-label">Suggestion</label>
-                <textarea type="text" class="form-control" id="suggestion"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
 
-    </div>
-    </div>
+        require 'phpmailer/src/Exception.php';
+        require 'phpmailer/src/PHPMailer.php';
+        require 'phpmailer/src/SMTP.php';
+
+
+
+        if (isset($_POST["send"])) {
+            $mail = new PHPMailer(true);
+
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'wzsohai@gmail.com';
+            $mail->Password = 'tidvjhvtjfmbkzma';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = '465';
+
+            $mail->setFrom('wzsohai@gmail.com');
+
+            $mail->addAddress('wzsohai@gmail.com');
+
+            $mail->isHTML(true);
+
+            $mail->Subject = $_POST["subject"];
+            $mail->Body = $_POST["message"] . " " . $_POST['email'];
+
+            $mail->send();
+
+            echo
+            "
+    
+            <script>
+    alert('Send Sucessfully');
+    document.location.herf = 'contact_us.php'
+    </script>
+    ";
+        }
+        ?>
+</div>
 </body>
-
-</html>
